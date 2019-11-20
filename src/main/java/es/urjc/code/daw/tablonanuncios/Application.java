fa242922aa40388fab1cbb0dda8c024ba5ac5a0a
cruizba <charlesrb94@outlook.com>
2019-11-20 13:56:21 +0000

    Added AWS Secret manager and updates. - Added securized to properties using aws secret manager - Updated app version - Updated spring boot version - Added profiles

--- src/main/java/es/urjc/code/daw/tablonanuncios/Application.java
+++ src/main/java/es/urjc/code/daw/tablonanuncios/Application.java
@@ -5,7 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.aws.jdbc.config.annotation.EnableRdsInstance;
 
 @SpringBootApplication
-@EnableRdsInstance(dbInstanceIdentifier="${cloud.aws.rds.dbInstanceIdentifier}", password="${cloud.aws.rds.springaws.password}")
+@EnableRdsInstance(dbInstanceIdentifier="${cloud.aws.rds.dbInstanceIdentifier}", databaseName="${cloud.aws.rds.springaws.databaseName}",
+	username="${cloud.aws.rds.springaws.username}", password="${cloud.aws.rds.springaws.password}")
 public class Application {
 
 	public static void main(String[] args) {
